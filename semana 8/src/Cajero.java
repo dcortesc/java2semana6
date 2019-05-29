d1c03b98891030b47403d9d1058af10c9aa08ef6
danielcortes <daniel@nekorp.com>
2019-05-29 11:11:42 -0300

    Carga Inicial

---
+++ semana 8/src/Cajero.java
@@ -0,0 +1,27 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author danielcortes
+ */
+public class Cajero {
+    
+    public static void main (String[]args){
+        final int saldoInicial=3000000;
+        int opcion;
+        
+        opcion = Integer.parseInt(JOptionPane.showInputDialog("Bienvenido al cajero de prueba de IACC\n"
+            +"1. Dépositos\n"
+            +"2. Giros\n"
+            +"3. Cancelar Operación\n"));
+        
+        
+    }
+}
